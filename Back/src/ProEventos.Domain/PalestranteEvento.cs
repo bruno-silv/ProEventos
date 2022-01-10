e9a3a1a95a3dfd7c88fb0052f21457224a376d40
Bruno <bruno_siilva2016@hotmail.com>
2022-01-10 18:06:44 -0300

    Seção 6: .NET Core - Orgarnizando Camadas

---
+++ Back/src/ProEventos.Domain/PalestranteEvento.cs
@@ -0,0 +1,16 @@
+using System;
+
+
+namespace ProEventos.Domain
+{
+    public class PalestranteEvento
+    {
+        public int PalestranteId { get; set; }
+
+        public Palestrante Palestrante { get; set; }
+
+        public int EventoId { get; set; }
+
+        public Evento Evento { get; set; }
+    }
+}
